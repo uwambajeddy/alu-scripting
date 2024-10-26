8fd55a491f60edf867a7c3811f6c5801ca7c9129
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-26 13:12:49 +0200

    fix bug

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,17 +8,17 @@ def top_ten(subreddit):
     if not return 0."""
 
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    subreddit_url = "https://reddit.com/r/{}.json?limit=10".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
-        json_data = response.json().get('data', {})
-        posts = json_data.get('children', [])
-        if posts:
-            for post in posts:
-                print(post.get('data', {}).get('title'))
-        else:
-            print("Ok")
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
-        
