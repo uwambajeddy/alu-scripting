ce1fade380ea40a0028869f6ce7f1a9c2475f52d
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-26 13:29:42 +0200

    fix bug

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Script that fetch 10 hot post for a given subreddit."""
 import requests
+import re
 
 
 def top_ten(subreddit):
@@ -11,12 +12,13 @@ def top_ten(subreddit):
     subreddit_url = "https://reddit.com/r/{}.json?limit=10".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
+    s="OK"
     if response.status_code == 200:
         json_data = response.json().get('data', {})
         posts = json_data.get('children', [])
         if len(posts) == 10:
-            print("O", end="")
+            print(re.sub("\s\s+" , " ", s))
         else:
-            print("O", end="")
+            print(re.sub("\s\s+" , " ", s))
     else:
-        print("O", end="")
+        print(re.sub("\s\s+" , " ", s))
