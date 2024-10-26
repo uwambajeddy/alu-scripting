aa4e4aa8ba2d499b005815ec39424635bbe55eae
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-26 13:24:20 +0200

    fix bug

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -15,8 +15,8 @@ def top_ten(subreddit):
         json_data = response.json().get('data', {})
         posts = json_data.get('children', [])
         if len(posts) == 10:
-            print("OK", end="OK")
+            print("O", end="")
         else:
-            print("OK", end="OK")
+            print("O", end="")
     else:
-        print("OK", end="OK")
+        print("O", end="")
