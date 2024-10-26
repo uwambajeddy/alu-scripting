db796967d258210855bbf859356fdde559195cf7
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-10-26 13:05:19 +0200

    fix bug

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""
-1-top_ten.py: Top Ten
-
-This module contains a function that queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit.
-"""
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the top ten hot posts for a given subreddit"""
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        json_data = response.json().get('data', {})
+        posts = json_data.get('children', [])
+        if posts:
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+    else:
         print(None)
-        return
-
-    data = response.json().get("data")
-    if data is None or len(data.get("children")) == 0:
-        print(None)
-        return
-
-    for child in data.get("children"):
-        print(child.get("data").get("title"))
-        
